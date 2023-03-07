c61e319160b19bfb5efaf302a49f73b30b71b80d
Borodur <60537405+Dalenir@users.noreply.github.com>
2023-03-07 22:25:16 +0400

    bip update

--- API/endpoints/pages.py
+++ API/endpoints/pages.py
@@ -1,10 +1,17 @@
-from fastapi import Request, APIRouter
+from fastapi import Request, APIRouter, WebSocket, Header
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from starlette.responses import FileResponse
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request, "sound": ["", 'https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3']})
+
+@router.get("/bips")
+async def home(request: Request, index: str = Header(default="8")):
+    dex = int(index) % 12
+    return FileResponse(f'./static/bips/{dex}.mp3', media_type='Audio')
---
+++ API/static/i_really_dk_js.js
@@ -0,0 +1,44 @@
+async function funk() {
+
+    let ind = parseInt(sessionStorage.getItem("ind")) ?? 1;
+
+    const ctx = new AudioContext();
+    let audio;
+    await fetch("/bips", { method: 'GET', headers: { 'index': ind.toString() }, cache: "no-cache"} )
+        .then(data => data.arrayBuffer())
+        .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+        .then(decodedAudio => {
+            audio = decodedAudio;
+        })
+
+    ind += 1
+    sessionStorage.setItem("ind", ind.toString());
+
+    function playback() {
+        const playSound = ctx.createBufferSource();
+        playSound.buffer = audio;
+        playSound.connect(ctx.destination);
+        playSound.start(ctx.currentTime);
+    }
+
+    playback()
+
+}
+
+async function copyUrl() {
+  try {
+    await navigator.clipboard.writeText(location.href);
+  } catch (err) {
+    console.error(err);
+  }
+  let text = document.getElementById("cptext");
+  text.classList.add("fade-in");
+  setTimeout(function () {
+    text.classList.remove("fade-in");
+  }, 1000);
+}
+
+
+document.querySelector("#boop_butt").addEventListener("click", funk)
+document.querySelector("#share_butt").addEventListener("click", copyUrl)
+
