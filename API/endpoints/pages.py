c4d68e0d5dff244d84000fa02a0ba5147dbe1593
Borodur <60537405+Dalenir@users.noreply.github.com>
2023-03-09 15:11:51 +0400

    auto ssl try

--- API/endpoints/pages.py
+++ API/endpoints/pages.py
@@ -9,9 +9,9 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "sound": ["", 'https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3']})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @router.get("/bips")
-async def home(request: Request, index: str = Header(default="8")):
+async def home(index: str = Header(default="1")):
     dex = int(index) % 12
     return FileResponse(f'./static/bips/{dex}.mp3', media_type='Audio')
